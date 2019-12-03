e522cecd100947e6567142b18ea2aa1d648b741d
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-03 11:26:09 +0100

    Chargement des images

---
+++ app/src/main/java/com/example/dogs/util/Util.kt
@@ -0,0 +1,29 @@
+package com.example.dogs.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.dogs.R
+
+// fonction qui affiche un spiner quand les images se chargent
+fun getProgessDrawable(context: Context) : CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 5f // épaisseur du trait
+        centerRadius = 25f // diamètre du cercle
+        start()
+    }
+}
+
+// fonction d'extension pour charger les images
+// dans l'adapter on pourra faire appel a cette fonction dans onBindViewholder() pour l'affichage
+fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_dog_icon)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+}
--- app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
+++ app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
@@ -7,6 +7,8 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogs.R
 import com.example.dogs.model.DogBreed
+import com.example.dogs.util.getProgessDrawable
+import com.example.dogs.util.loadImage
 import kotlinx.android.synthetic.main.item_dog.view.*
 
 class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
@@ -35,6 +37,7 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<D
         holder.view.setOnClickListener {
             Navigation.findNavController(it).navigate(ListDogsFragmentDirections.actionDetailDogFragment())
         }
+        holder.view.imageView.loadImage(dogsList[position].imageUrl, getProgessDrawable(holder.view.imageView.context))
     }
 
     class DogViewHolder(var view: View): RecyclerView.ViewHolder(view)
